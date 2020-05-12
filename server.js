7e508a96f66d5f7916b1df3352a943b51187b4ce
Sergio Di Martino <sergio@webdevserg.io>
2020-05-12 10:42:49 -0700

    Begins adding routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/redux_todo_db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/redux_todo_db', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.listen(PORT);
