7a6fd84480f0eb134bae1fe96e980ff6bbe2d458
Sergio Di Martino <sergio@webdevserg.io>
2020-05-12 12:32:52 -0700

    Uncommented my routes, duh

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const app = express();
 
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/redux_todo_db', {useNewUrlParser: true, useUnifiedTopology: true});
 
