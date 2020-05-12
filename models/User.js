63e813e996f469ecb4a348e2ce58c1d6238e9fa9
Sergio Di Martino <sergio@webdevserg.io>
2020-05-12 11:44:17 -0700

    Adds User schema

--- models/User.js
+++ models/User.js
@@ -1,2 +1,33 @@
 const { Schema, model } = require('mongoose');
 const { isEmail, isLength } = require('validator');
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: [
+      true,
+      'Error: Email address is required'
+    ],
+    validate: [
+      isEmail,
+      'Error: Please enter a valid email address'
+    ]
+  },
+  password: {
+    type: String,
+    required: [
+      true,
+      'Error: Password is required'
+    ],
+    validate: [
+      (value) => isLength(value, { min: 6 }),
+      'Error: Password must be at least 6 characters long'
+    ]
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now()
+  }
+})
