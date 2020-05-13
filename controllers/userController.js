ebffb5cfab9e0812e4f87bfc98ee94ea362ef690
Sergio Di Martino <sergio@webdevserg.io>
2020-05-13 10:08:12 -0700

    Adds getAllUserEmails route

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,5 +16,16 @@ module.exports = {
     } catch (e) {
       return res.status(403).json(e);
     }
+  },
+  getAllUserEmails: async (req, res) => {
+    try {
+      const users = await User.find()
+      if (!users) {
+        return res.status(404).json({ error: 'No users yet' });
+      }
+      return res.status(200).json(users);
+    } catch (e) {
+      return res.status(403).json(e);
+    }
   }
 }
--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
-const { addTodo } = require('./../../../controllers/userController');
+const { addTodo, getAllUserEmails } = require('./../../../controllers/userController');
 
 router.route('/todos')
   .post(addTodo);
+
+router.get('/emails', getAllUserEmails);
 
 module.exports = router;
