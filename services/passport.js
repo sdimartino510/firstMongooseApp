fb7ac55ab5bfe4840a7c1303c3b29960b2fdea79
Sergio Di Martino <sergio@webdevserg.io>
2020-05-13 13:28:16 -0700

    Adds localLogin

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,8 @@
 const passport = require('passport');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const LocalStrategy = require('passport-local');
+
 const { User } = require('./../models');
-
-
 const { secret } = require('./../config');
 
 // Setup options for JWT
@@ -16,6 +16,26 @@ const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
   try {
     const user = await User.findById(payload.sub);
     if (!user) {
+      return done(null, false);
+    }
+    return done(null, user);
+  } catch (e) {
+    return done(e, false);
+  }
+});
+
+const localOptions = {
+  usernameField: 'email'
+}
+
+const localLogin = new LocalStrategy(localOptions, async (email, password, done) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return done(null, false);
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
       return done(null, false);
     }
     return done(null, user);
