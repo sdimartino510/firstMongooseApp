ee19b907c7125f166cd8ced1fc5866634ac99cff
Sergio Di Martino <sergio@webdevserg.io>
2020-05-13 12:13:59 -0700

    Removes auth requirement from getAllUserEmails

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const { addTodo, getAllUserEmails } = require('./../../../controllers/userController');
 
-const { requireAuth } = require('./../../../middleware/authMiddleware');
+// const { requireAuth } = require('./../../../middleware/authMiddleware');
 
 router.route('/todos')
   .post(addTodo);
 
-router.get('/emails', requireAuth, getAllUserEmails);
+router.get('/emails', getAllUserEmails);
 
 module.exports = router;
