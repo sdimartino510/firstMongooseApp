4d4aa46ed43e97f277be56bbe4b3e1a02555b9cf
Sergio Di Martino <sergio@webdevserg.io>
2020-05-14 10:18:49 -0700

    Adds getTodo controller and route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-// const todoRoutes = require('./todoRoutes');
+const todoRoutes = require('./todoRoutes');
 const userRoutes = require('./userRoutes');
 const authRoutes = require('./authRoutes');
 
-// router.use('/todo', todoRoutes);
+router.use('/todo', todoRoutes);
 router.use('/user', userRoutes);
 router.use('/auth', authRoutes);
 
